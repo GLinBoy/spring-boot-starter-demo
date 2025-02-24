07f6ada995ea7b12884ddcafa75329fc3d80076c
GLinBoy <glinboy@live.com>
2025-02-24 20:44:00 +0100

    📦 NEW: Add a sample project to use jps-spring-boot-starter

---
+++ hello-jps/src/main/java/com/glinboy/demo/jps/hellojps/HelloJpsApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.demo.jps.hellojps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJpsApplication.class, args);
+	}
+
+}
---
+++ hello-jps/src/test/java/com/glinboy/demo/jps/hellojps/HelloJpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glinboy.demo.jps.hellojps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
