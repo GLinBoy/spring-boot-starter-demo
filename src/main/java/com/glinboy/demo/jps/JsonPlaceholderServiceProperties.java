3ae1c8bbac299a65401b2f3855b90074faa230bb
GLinBoy <glinboy@live.com>
2025-02-24 20:32:27 +0100

    📦 NEW: Add properties holder

---
+++ src/main/java/com/glinboy/demo/jps/JsonPlaceholderServiceProperties.java
@@ -0,0 +1,11 @@
+package com.glinboy.demo.jps;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@ConfigurationProperties("json-placeholder-service")
+public record JsonPlaceholderServiceProperties(
+		@DefaultValue("https://jsonplaceholder.typicode.com")
+		String baseUrl) {
+
+}
